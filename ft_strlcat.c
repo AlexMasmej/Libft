353087d5964c8d7aef684c784e1599f3baeac980
Alexandre Masmejean <amasmeje@e1z3r1p17.42.us.org>
2019-08-08 15:17:23 -0700

    lol

---
+++ ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amasmeje <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/07 21:24:07 by amasmeje          #+#    #+#             */
+/*   Updated: 2019/08/08 15:14:09 by amasmeje         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dst[i])
+		i++;
+	while (src[j])
+		j++;
+	return ();
+}
+
+int		main()
+{
+	char dst[20] = "Hello bitches";
+	char src[20] = "what's up suckaz";
+	printf("%lu", strlcat(dst, src, 5));
+	return (0);
+}
