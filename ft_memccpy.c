fd65f7c57b129ddbe161bd249c029a05fb3e91f2
Alexandre Masmejean <amasmeje@e1z3r1p17.42.us.org>
2019-08-12 22:30:55 -0700

    MEM

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,23 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amasmeje <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/05 17:00:22 by amasmeje          #+#    #+#             */
-/*   Updated: 2019/08/12 21:37:47 by amasmeje         ###   ########.fr       */
+/*   Created: 2019/08/12 21:36:44 by amasmeje          #+#    #+#             */
+/*   Updated: 2019/08/12 21:36:47 by amasmeje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+void	*ft_memccpy(void *dst, void *src, int c, size_t n)
 {
-	size_t i;
+	size_t	i;
+	char	*dst1;
+	char	*src1;
 
 	i = 0;
-	while (s[i])
+	dst1 = (char *)dst;
+	src1 = (char *)src;
+	while (i < n)
+	{
+		if (src1[i] == c)
+		{
+			dst1[i] = src1[i];
+			return (dst++);
+		}
+		dst1[i] = src1[i];
 		i++;
-	return (i);
+	}
+	return (NULL);
 }
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,23 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amasmeje <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/05 17:00:22 by amasmeje          #+#    #+#             */
-/*   Updated: 2019/08/12 21:37:47 by amasmeje         ###   ########.fr       */
+/*   Created: 2019/08/12 21:36:28 by amasmeje          #+#    #+#             */
+/*   Updated: 2019/08/12 21:36:29 by amasmeje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
+	size_t	i;
+	char	*dst1;
+	char	*src1;
 
 	i = 0;
-	while (s[i])
+	dst1 = (char *)dst;
+	src1 = (char *)src;
+	while (i < n)
+	{
+		dst1[i] = src1[i];
 		i++;
-	return (i);
+	}
+	return (dst);
 }
