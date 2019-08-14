a8e10b653f944dae8d4e7d5568ce7376bbfa27e1
Alexandre Masmejean <amasmeje@e1z3r1p17.42.us.org>
2019-08-13 21:45:14 -0700

    memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -11,3 +11,20 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*s1;
+
+	i = 0;
+	s1 = (char *)s;
+	while (s1[i])
+	{
+		if (s1[i] == c && i < n)
+			return ((char *)s);
+		s++;
+		i++;
+	}
+	return (NULL);
+}
