d42aff7d3065025897003fa96cc9118650e75687
Alexandre Masmejean <amasmeje@e1z3r1p17.42.us.org>
2019-08-13 22:28:52 -0700

    lol

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -11,3 +11,26 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	size_t	j;
+	unsigned char	*u1;
+	unsigned char	*u2;
+
+	u1 = (unsigned char *)s1;
+	u2 = (unsigned char *)s2;
+	i = 0;
+	j = 0;
+	while (u1 && u2 && i < n)
+	{
+		if (u1[i] != u2[i])
+		{
+			j = (unsigned char)u1[i] - (unsigned char)u2[i];
+			break;
+		}
+		i++;
+	}
+	return (j);
+}
