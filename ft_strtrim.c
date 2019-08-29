4434a1b4cf5f5723f5465ab27c02ddcd29889829
Alexandre Masmejean <amasmeje@e1z3r2p1.42.us.org>
2019-08-29 15:47:02 -0700

    lol

---
+++ ft_strtrim.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amasmeje <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/29 13:46:40 by amasmeje          #+#    #+#             */
+/*   Updated: 2019/08/29 13:46:48 by amasmeje         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	size_t		i;
+	size_t		j;
+	size_t		k;
+	char		*s1;
+
+	i = 0;
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		i++;
+	j = ft_strlen(s) - 1;
+	while ((s[j] == ' ' || s[j] == '\t' || s[j] == '\n') && i != ft_strlen(s))
+		j--;
+	if (!(s1 = (char *)malloc(j - i + 1)))
+		return (NULL);
+	k = 0;
+	while (i <= j)
+	{
+		s1[k] = s[i];
+		k++;
+		i++;
+	}
+	s1[k] = '\0';
+	return (s1);
+}
