c6302f699e2a14cf402b5757f801e747e201c0c7
Alexandre Masmejean <amasmeje@e1z3r2p1.42.us.org>
2019-08-29 13:46:01 -0700

    part 2 still

---
+++ ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amasmeje <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/29 13:35:57 by amasmeje          #+#    #+#             */
+/*   Updated: 2019/08/29 13:36:08 by amasmeje         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s3;
+	int		i;
+	int		j;
+
+	if (!(s3 = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		s3[i] = s2[j];
+		i++;
+		j++;
+	}
+	s3[i] = '\0';
+	return (s3);
+}
---
+++ ft_strsub.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amasmeje <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/29 13:03:04 by amasmeje          #+#    #+#             */
+/*   Updated: 2019/08/29 13:03:15 by amasmeje         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*s1;
+	size_t	i;
+
+	i = 0;
+	if (!(s1 = (char *)malloc(len + 1)))
+		return (NULL);
+	while (i < len)
+	{
+		s1[i] = s[start];
+		start++;
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
