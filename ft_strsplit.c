d039d730d8e6cce166af2221b1db86fb6278a02a
Alexandre Masmejean <amasmeje@e1z3r5p1.42.us.org>
2019-08-31 22:19:32 -0700

    strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -12,17 +12,66 @@
 
 #include "libft.h"
 
-char	**ft_strsplit(char const *s, char c)
+int		count_words(const char *s, int c)
 {
 	int		i;
-	int		nstring;
-	char	*array[nstring];
+	int		n;
 
+	i = 0;
+	n = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			i++;
+			continue;
+		}
+		while (s[i] != c && s[i])
+			i++;
+		n++;
+	}
+	return (n);
+}
+
+int		get_word_len(const char *s, char c)
+{
+	int		i;
+	int		len;
+
+	len = 0;
 	i = 0;
 	while (s[i] == c)
 		i++;
-	while (s[i] != c)
+	while (s[i] != c && s[i])
 	{
-		
+		i++;
+		len++;
 	}
+	return (len);
+}
+
+char	**ft_strsplit(char const *s, char c)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**array;
+
+	i = -1;
+	j = 0;
+	if (!(array = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1))))
+		return (NULL);
+	while (++i < count_words(s, c))
+	{
+		k = 0;
+		if (!(array[i] = (char *)malloc(get_word_len(&s[j], c) + 1)))
+			return (NULL);
+		while (s[j] == c)
+			j++;
+		while (s[j] != c && s[j])
+			array[i][k++] = s[j++];
+		array[i][k] = '\0';
+	}
+	array[i] = NULL;
+	return (array);
 }
